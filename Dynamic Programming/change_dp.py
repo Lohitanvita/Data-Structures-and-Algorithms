c8ff49849cc0dcc1634abab4d951b026e05d4dde
Lohitanvita <87051664+Lohitanvita@users.noreply.github.com>
2021-09-16 16:00:06 -0400

    Add files via upload

---
+++ Dynamic Programming/change_dp.py
@@ -0,0 +1,18 @@
+import sys
+
+
+def get_change(money, coins):
+    MinNumCoins = []
+    for m in range(money + 1):
+        MinNumCoins.append(m)
+        for i in range(len(coins)):
+            if m >= coins[i]:
+                NumCoins = MinNumCoins[m - coins[i]] + 1
+                if NumCoins < MinNumCoins[m]:
+                    MinNumCoins[m] = NumCoins
+    return MinNumCoins[money]
+
+
+if __name__ == '__main__':
+    m = int(input())
+    print(get_change(m,[1,3,4]))
